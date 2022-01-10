834ad79df0ebc5bedf5882f614f6b000ee81dfc9
kast96 <kast96@yandex.ru>
2022-01-10 20:10:47 +0300

    save design

--- src/components/Game/Popups/LoadGame/LoadGame.jsx
+++ src/components/Game/Popups/LoadGame/LoadGame.jsx
@@ -3,8 +3,13 @@ import s from './LoadGame.module.scss';
 const LoadGame = ({onSetActivePopup}) => {
     return (
         <div className={s.popup}>
-            <div>Load</div>
-            <div onClick={onSetActivePopup.bind(this, null)}>X</div>
+            <div className={s.popup__header}>
+                <h1 className={s.popup__title}>Load</h1>
+                <div className={s.popup__close} onClick={onSetActivePopup.bind(this, null)}>X</div>
+            </div>
+            <div className={s.popup__body}>
+
+            </div>
         </div>
     );
 }
--- src/components/Game/Popups/SaveGame/SaveGame.jsx
+++ src/components/Game/Popups/SaveGame/SaveGame.jsx
@@ -1,10 +1,29 @@
 import s from './SaveGame.module.scss';
+import {ReactComponent as CloseSvg} from '../../../../resources/svg/times-solid.svg';
 
 const SaveGame = ({onSetActivePopup}) => {
+    let items = [];
+    for (let i = 1; i <= 6; i++) {
+        items.push(i);
+    }
     return (
         <div className={s.popup}>
-            <div>Save</div>
-            <div onClick={onSetActivePopup.bind(this, null)}>X</div>
+            <div className={s.popup__header}>
+                <h1 className={s.popup__title}>Save</h1>
+                <div className={s.popup__close} onClick={onSetActivePopup.bind(this, null)}><CloseSvg /></div>
+            </div>
+            <div className={s.popup__body}>
+                <div className={s.popup__items}>
+                    {items.map(item => (
+                    <div className={s.popup__item}>
+                        <div className={s.popup__item__number}>{item}</div>
+                        <div className={s.popup__item__name}>Save {item}</div>
+                        <div className={s.popup__item__progress  + ' green'}>34%</div>
+                        <div className={s.popup__item__date}>10.01.2022</div>
+                    </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
